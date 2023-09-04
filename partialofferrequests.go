9a993e29ce295a69a642727b977603bbff3f27d6
0x9ef <0x9ef@tutanota.com>
2023-09-04 13:13:57 +0300

    feat(flights): add partial offer requests functionality (#7)
    * feat(flights): add partial offer requests functionality
    
    * fix(flights): add missing test for ListPartialOfferRequestFares
    
    * fix(flights): fix incorrect params encodings
    
    * chore: delete comment

---
+++ partialofferrequests.go
@@ -0,0 +1,93 @@
+package duffel
+
+import (
+	"context"
+	"net/url"
+)
+
+const partialOsfferRequestIDPrefix = "prq_"
+
+type (
+	PartialOfferRequestClient interface {
+		CreatePartialOfferRequest(ctx context.Context, requestInput PartialOfferRequestInput, requestOptions ...RequestOption) (*OfferRequest, error)
+		GetPartialOfferRequest(ctx context.Context, partialOfferRequestID string, options []GetPartialOfferRequestParams, requestOptions ...RequestOption) (*OfferRequest, error)
+		ListPartialOfferRequestFares(ctx context.Context, partialOfferRequestID string, options []ListPartialOfferRequestFaresParams, requestOptions ...RequestOption) (*OfferRequest, error)
+	}
+
+	PartialOfferRequestInput struct {
+		// The passengers who want to travel. If you specify an age for a passenger,
+		// the type may differ for the same passenger in different offers due to airline's different rules.
+		// e.g. one airline may treat a 14 year old as an adult, and another as a young adult.
+		// You may only specify an age or a type – not both.
+		Passengers []OfferRequestPassenger `json:"passengers" url:"-"`
+		// The slices that make up this offer request. One-way journeys can be expressed using one slice,
+		// whereas return trips will need two.
+		Slices []OfferRequestSlice `json:"slices" url:"-"`
+		// The cabin that the passengers want to travel in
+		CabinClass CabinClass `json:"cabin_class" url:"-"`
+		// The maximum number of connections within any slice of the offer.
+		// For example 0 means a direct flight which will have a single segment within each slice
+		// and 1 means a maximum of two segments within each slice of the offer.
+		MaxConnections *int `json:"max_connections,omitempty" url:"-"`
+		// The maximum amount of time in milliseconds to wait for each airline to respond
+		SupplierTimeout int `json:"-" url:"supplier_timeout,omitempty"`
+	}
+
+	GetPartialOfferRequestParams struct {
+		SelectedPartialOffer string `url:"selected_partial_offer"`
+	}
+
+	ListPartialOfferRequestFaresParams struct {
+		SelectedPartialOffer string `url:"selected_partial_offer"`
+	}
+)
+
+func (a *API) CreatePartialOfferRequest(ctx context.Context, requestInput PartialOfferRequestInput, requestOptions ...RequestOption) (*OfferRequest, error) {
+	return newRequestWithAPI[PartialOfferRequestInput, OfferRequest](a).
+		Post("/air/partial_offer_requests", &requestInput).
+		WithOptions(requestOptions...).
+		Single(ctx)
+}
+
+func (a *API) GetPartialOfferRequest(ctx context.Context, partialOfferRequestID string, params []GetPartialOfferRequestParams, requestOptions ...RequestOption) (*OfferRequest, error) {
+	if err := validateID(partialOfferRequestID, partialOsfferRequestIDPrefix); err != nil {
+		return nil, err
+	}
+
+	return newRequestWithAPI[GetPartialOfferRequestParams, OfferRequest](a).
+		Getf("/air/partial_offer_requests/%s", partialOfferRequestID).
+		WithParams(normalizeParams(params)...).
+		WithOptions(requestOptions...).
+		Single(ctx)
+}
+
+func (a *API) ListPartialOfferRequestFares(ctx context.Context, partialOfferRequestID string, params []ListPartialOfferRequestFaresParams, requestOptions ...RequestOption) (*OfferRequest, error) {
+	if err := validateID(partialOfferRequestID, partialOsfferRequestIDPrefix); err != nil {
+		return nil, err
+	}
+
+	return newRequestWithAPI[ListPartialOfferRequestFaresParams, OfferRequest](a).
+		Getf("/air/partial_offer_requests/%s/fares", partialOfferRequestID).
+		WithParams(normalizeParams(params)...).
+		WithOptions(requestOptions...).
+		Single(ctx)
+}
+
+var _ PartialOfferRequestClient = (*API)(nil)
+
+func (o GetPartialOfferRequestParams) Encode(q url.Values) error {
+	if o.SelectedPartialOffer != "" {
+		q.Set("selected_partial_offer[]", o.SelectedPartialOffer)
+	}
+	return nil
+}
+
+func (o ListPartialOfferRequestFaresParams) Encode(q url.Values) error {
+	q.Add("selected_partial_offer[]", o.SelectedPartialOffer)
+	return nil
+}
+
+// Encode implements the ParamEncoder interface.
+func (o PartialOfferRequestInput) Encode(q url.Values) error {
+	return nil
+}
---
+++ partialofferrequests_test.go
@@ -0,0 +1,118 @@
+package duffel
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/h2non/gock.v1"
+)
+
+func TestCreatePartialOfferRequest(t *testing.T) {
+	defer gock.Off()
+	a := assert.New(t)
+	gock.New("https://api.duffel.com").
+		Post("/air/partial_offer_requests").
+		Reply(201).
+		SetHeader("Ratelimit-Limit", "5").
+		SetHeader("Ratelimit-Remaining", "5").
+		SetHeader("Ratelimit-Reset", time.Now().Format(time.RFC1123)).
+		SetHeader("Date", time.Now().Format(time.RFC1123)).
+		File("fixtures/201-create-partial-offer-request.json")
+
+	ctx := context.TODO()
+
+	client := New("duffel_test_123")
+	data, err := client.CreatePartialOfferRequest(ctx, PartialOfferRequestInput{
+		Passengers: []OfferRequestPassenger{
+			{
+				FamilyName: "Earhardt",
+				GivenName:  "Amelia",
+				Type:       PassengerTypeAdult,
+			},
+			{
+				Age: 14,
+			},
+		},
+		CabinClass: CabinClassEconomy,
+		Slices: []OfferRequestSlice{
+			{
+				DepartureDate: Date(time.Now().AddDate(0, 0, 7)),
+				Origin:        "NYC",
+				Destination:   "ATL",
+			},
+			{
+				DepartureDate: Date(time.Now().AddDate(0, 0, 9)),
+				Origin:        "ATL",
+				Destination:   "NYC",
+			},
+		},
+	})
+	a.NoError(err)
+	a.NotNil(data)
+
+	a.Equal("973.73 GBP", data.Offers[0].TotalAmount().String())
+	a.Equal("148.54 GBP", data.Offers[0].TaxAmount().String())
+	a.Equal("city", data.Slices[0].OriginType)
+	a.Len(data.Slices, 2) // inbound and outbound
+}
+
+func TestGetPartialOfferRequest(t *testing.T) {
+	defer gock.Off()
+	a := assert.New(t)
+	gock.New("https://api.duffel.com").
+		Get("/air/partial_offer_requests/prq_0000AZPy1jdXi7327O8H9k").
+		Reply(200).
+		SetHeader("Ratelimit-Limit", "5").
+		SetHeader("Ratelimit-Remaining", "5").
+		SetHeader("Ratelimit-Reset", time.Now().Format(time.RFC1123)).
+		SetHeader("Date", time.Now().Format(time.RFC1123)).
+		File("fixtures/200-get-partial-offer-request.json")
+
+	ctx := context.TODO()
+
+	client := New("duffel_test_123")
+	data, err := client.GetPartialOfferRequest(ctx, "prq_0000AZPy1jdXi7327O8H9k", []GetPartialOfferRequestParams{
+		{
+			SelectedPartialOffer: "off_0000AZPy1jzsN4udEea6yM_0",
+		},
+	})
+	a.NoError(err)
+	a.NotNil(data)
+	a.Equal("973.73 GBP", data.Offers[0].TotalAmount().String())
+	a.Equal("148.54 GBP", data.Offers[0].TaxAmount().String())
+	a.Equal("airport", data.Offers[0].Slices[0].DestinationType)
+	a.Len(data.Slices, 2)
+}
+
+func TestListPartialOfferRequestFares(t *testing.T) {
+	defer gock.Off()
+	a := assert.New(t)
+	gock.New("https://api.duffel.com").
+		Get("/air/partial_offer_requests/prq_0000AZPy1jdXi7327O8H9k/fares").
+		MatchParam("selected_partial_offer", "off_0000AZPy1jzsN4udEea6yM_0").
+		MatchParam("selected_partial_offer", "off_0000AZPy1jzsN4udEea6yM_1").
+		Reply(200).
+		SetHeader("Ratelimit-Limit", "5").
+		SetHeader("Ratelimit-Remaining", "5").
+		SetHeader("Ratelimit-Reset", time.Now().Format(time.RFC1123)).
+		SetHeader("Date", time.Now().Format(time.RFC1123)).
+		File("fixtures/200-list-partial-offer-request-fares.json")
+
+	ctx := context.TODO()
+
+	client := New("duffel_test_123")
+	data, err := client.ListPartialOfferRequestFares(ctx, "prq_0000AZPy1jdXi7327O8H9k",
+		[]ListPartialOfferRequestFaresParams{
+			{SelectedPartialOffer: "off_0000AZPy1jzsN4udEea6yM_0"}, {SelectedPartialOffer: "off_0000AZPy1jzsN4udEea6yM_1"},
+		})
+	a.NoError(err)
+	a.NotNil(data)
+	a.Equal("273.22 GBP", data.Offers[0].TotalAmount().String())
+	a.Equal("41.68 GBP", data.Offers[0].TaxAmount().String())
+	// Second offer
+	a.Equal("337.78 GBP", data.Offers[1].TotalAmount().String())
+	a.Equal("51.53 GBP", data.Offers[1].TaxAmount().String())
+	a.Len(data.Slices, 2)
+}
